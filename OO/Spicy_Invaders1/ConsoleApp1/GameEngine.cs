f1a1e6425954c760a1610fe5b323630f26e98375
ethanschafstall <ethans9999@gmail.com>
2023-10-11 17:09:29 +0200

    Renamed GameLogic to GameEngine
    Renamed gamelogic class to gameengine

--- OO/Spicy_Invaders1/ConsoleApp1/GameEngine.cs
+++ OO/Spicy_Invaders1/ConsoleApp1/GameEngine.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualBasic;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,185 +7,257 @@ using System.Threading.Tasks;
 namespace Spicy_Invaders
 {
     /// <summary>
-    /// GameEngine Handles all visual aspects of the game. 
+    /// GameLogic class the handles all game behaviours/calculations.
     /// </summary>
     public class GameEngine
     {
-        private static List<string> _strawberrySprite;
 
-        private static List<ConsoleColor> _strawberryColors;
+        public List<Enemy> Enemies { get; }
+        public List<Projectile> Projectiles { get; }
+        public PlayerShip PlayerShip { get; }
 
-        private static List<string> _bananaSprite;
-
-        private static List<ConsoleColor> _bananaColors;
-
-        private static List<string> _grapeSprite;
-
-        private static List<ConsoleColor> _grapeColors;
-
-        private static List<string> _melonSprite;
-
-        private static List<ConsoleColor> _melonColors;
-
-        private static List<string> _pepperSprite;
-
-        private static List<ConsoleColor> _pepperColors;
-
-        private static List<string> _missileSpriteDown;
-
-        private static List<ConsoleColor> _missileColorsDown;
-
-        private static List<string> _missileSpriteUp;
-
-        private static List<ConsoleColor> _missileColorsUp;
-        static GameEngine()
+        public GameEngine()
         {
-            _strawberrySprite = new List<string> { " w ", "\\ /" };
-
-            _strawberryColors = new List<ConsoleColor> { ConsoleColor.Red, ConsoleColor.Green };
-
-            _bananaSprite = new List<string> { "  ,", " /|", "/ /", "// " };
-
-            _bananaColors = new List<ConsoleColor> { ConsoleColor.DarkYellow, ConsoleColor.DarkGreen };
-
-            _grapeSprite = new List<string> { " ? ", "OoO", "oOo", " o " };
-
-            _grapeColors = new List<ConsoleColor> { ConsoleColor.DarkMagenta, ConsoleColor.Green };
-
-            _melonSprite = new List<string> { "/¯T¯\\", "| | |", "\\_|_/" };
-
-            _melonColors = new List<ConsoleColor> { ConsoleColor.DarkGreen, ConsoleColor.DarkGreen };
-
-            _pepperSprite = new List<string> { "/\\ ", "// ", "J  " };
-
-            _pepperColors = new List<ConsoleColor> { ConsoleColor.Red, ConsoleColor.DarkGreen };
-
-            _missileSpriteDown = new List<string> { "*", "|", "v" };
-
-            _missileSpriteUp = new List<string> { "^", "|", "*" };
-
-            _missileColorsDown = new List<ConsoleColor> { ConsoleColor.DarkYellow, ConsoleColor.DarkGray, ConsoleColor.Red};
-
-            _missileColorsUp = new List<ConsoleColor> { ConsoleColor.Red, ConsoleColor.DarkGray, ConsoleColor.DarkYellow };
+            Enemies = new List<Enemy>();
+            Projectiles = new List<Projectile>();
+            PlayerShip = new PlayerShip(GameSettings.PLAYER_START_POS.X, GameSettings.PLAYER_START_POS.Y);
         }
 
-        public static void Init()
+        /// <summary>
+        /// Method responsible for creating new enemy objects and adding them to the Enemies list. 
+        /// </summary>
+        /// <param name="isMelon">bool if is melon enemy is to be spawned</param>
+        public void SpawnEnemy(bool isMelon = false)
         {
-            Console.CursorVisible = false;
-            Console.SetWindowSize(GameSettings.WINDOW_WIDTH, GameSettings.WINDOW_HEIGHT);
-            Console.SetBufferSize(GameSettings.WINDOW_WIDTH,GameSettings.WINDOW_HEIGHT);
-        }
-        public static void Clear()
-        {
-            Console.Clear();
-        }
-        public static void DrawEnemies(List<Enemy> enemies)
-        {
-            List<string> currentEnemySprite = new List<string> { };
-            List<ConsoleColor> currentEnemyColors = new List<ConsoleColor> { };
-            if (enemies == null) return;
-            for (int i = 0; i < enemies.Count; i++)
+            if (!isMelon)
             {
-                switch (enemies[i].enemyType)
+                Random random = new Random();
+                EnemyType randomType = (EnemyType)random.Next(1, 4);
+                int yPosition = 0;
+                int yVelocity = 0;
+                switch (randomType)
                 {
                     case EnemyType.Strawberry:
-                        currentEnemySprite = _strawberrySprite;
-                        currentEnemyColors = _strawberryColors;
-                        break;
-                    case EnemyType.Melon:
-                        currentEnemySprite = _melonSprite;
-                        currentEnemyColors = _melonColors;
+                        yPosition = 0;
+                        yVelocity = 5;
                         break;
                     case EnemyType.Banana:
-                        currentEnemySprite = _bananaSprite;
-                        currentEnemyColors = _bananaColors;
+                        yPosition = 0;
+                        yVelocity = 5;
                         break;
                     case EnemyType.Grape:
-                        currentEnemySprite = _grapeSprite;
-                        currentEnemyColors = _grapeColors;
+                        yPosition = 0;
+                        yVelocity = 5;
                         break;
                 }
-                for (int j = 0; j < currentEnemySprite.Count; j++)
+                Enemy spawnedEnemy = new Enemy(GameSettings.ENEMY_START_POS.X, GameSettings.ENEMY_START_POS.Y + yPosition, randomType, Direction.Right);
+                spawnedEnemy.Velocity = new Vector((int)GameSettings.ENEMYVELOCITY, yVelocity);
+                Enemies.Add(spawnedEnemy);
+            }
+            else
+            {
+                Enemy spawnedMelon = new Enemy(GameSettings.ENEMY_START_POS.X, GameSettings.ENEMY_START_POS.Y, EnemyType.Melon, Direction.Right);
+                spawnedMelon.Velocity = new Vector(3, 3);
+                Enemies.Add(spawnedMelon);
+            }
+        }
+
+        /// <summary>
+        /// Method responsible for moving enemy objects from Enemies list, and removing them from said list, based on enemy move direction and gameboard limits.
+        /// </summary>
+        /// <returns> Returns true if an enemy has reached bottom gameboard limit, otherwise returns false</returns>
+        public bool MoveEnemy()
+        {
+            if (Enemies == null) return false;
+
+            for (int i = 0; i < Enemies.Count; i++)
+            {
+                if (Enemies[i].Position.X + Enemies[i].Velocity.X > GameSettings.GAMEBOARD_X_LIMIT && Enemies[i].TravelDirection == Direction.Right)
                 {
-                    switch (j)
+                    Enemies[i].TravelDirection = Direction.Left;
+                    Enemies[i].Position.Y = Enemies[i].Position.Y + Enemies[i].Velocity.Y;
+                }
+                if (Enemies[i].Position.X - Enemies[i].Velocity.X <= GameSettings.GAMEBOARD_X_START && Enemies[i].TravelDirection == Direction.Left)
+                {
+                    Enemies[i].TravelDirection = Direction.Right;
+                    Enemies[i].Position.Y = Enemies[i].Position.Y + Enemies[i].Velocity.Y;
+                }
+                if (Enemies[i].Position.Y + Enemies[i].Velocity.Y > GameSettings.GAMEBOARD_Y_LIMIT)
+                {
+                    Enemies.Remove(Enemies[i]);
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < Enemies.Count; i++)
+            {
+                Enemies[i].Move();
+            }
+                return false;
+        }
+
+        /// <summary>
+        /// Method responsible for moving projectile objects from Projectiles list, and removing them from said list. Based on projectile move direction and gameboard limits
+        /// </summary>
+        public void MoveProjectile()
+        {
+            if (Projectiles == null) return;
+
+            for (int i = 0; i < Projectiles.Count; i++)
+            {
+                if (Projectiles[i].Position.Y + Projectiles[i].Velocity.Y > GameSettings.GAMEBOARD_Y_LIMIT && Projectiles[i].TravelDirection == Direction.Down)
+                {
+                    Projectiles.RemoveAt(i);
+                }
+                else if (Projectiles[i].Position.Y - Projectiles[i].Velocity.Y <= 0 && Projectiles[i].TravelDirection == Direction.Up)
+                {
+                    Projectiles.RemoveAt(i);
+                }
+            }
+
+            for (int i = 0; i < Projectiles.Count; i++)
+            {
+                Projectiles[i].Move();
+            }
+        }
+
+        /// <summary>
+        /// Method responsible for checking projectile positions and comparing them with the enemy/player positions to see if there is a collision. 
+        /// </summary>
+        /// <returns>Returns true if the player was hit, otherwise false</returns>
+        public void ProjectileCollisionDetection()
+        {
+            if (Projectiles == null) return;
+
+            for (int i = 0; i < Projectiles.Count; i++)
+            {
+                if (Projectiles[i].TravelDirection == Direction.Up)
+                {
+                    for (int j = 0; j < Enemies.Count; j++)
                     {
-                        default:
-                            Console.ForegroundColor = currentEnemyColors[0];
-                            break;
-                        case 0:
-                            Console.ForegroundColor = currentEnemyColors[1];
-                            break;
+                        bool yAxisCondition = false;
+
+                        if (Projectiles[i] is    Laser)
+                        // Because the laser travels at a faster speed than other projectile types the standard condition
+                        // doesn't work as intended so there is a special condition only for the Laser type.
+                        {
+                            yAxisCondition = Projectiles[i].Position.Y <= Enemies[j].Position.Y+3 || Projectiles[i].Position.Y - Projectiles[i].Velocity.Y < Enemies[j].Position.Y+3;
+                        }
+                        else
+                        {
+                            yAxisCondition = Projectiles[i].Position.Y <= Enemies[j].Position.Y+3;
+                        }
+
+                        if (!(Enemies[j].enemyType == EnemyType.Melon))
+                        // if the enemy being shot at is not a melon, as melons are bigger so need a different
+                        {
+                            if (yAxisCondition &&
+                                Projectiles[i].Position.X >= Enemies[j].Position.X &&
+                                Projectiles[i].Position.X <= Enemies[j].Position.X + 3)
+                                // if the projectile x and y postions intersect with the enemy x and y postions then remove both from the list
+                            {
+                                Enemies[j].Hit(Projectiles[i]);
+                                Projectiles.RemoveAt(i);
+                                break;
+                            }
+
+                        }
+                        else
+                        {
+                            if (yAxisCondition &&
+                                Projectiles[i].Position.X >= Enemies[j].Position.X &&
+                                Projectiles[i].Position.X <= Enemies[j].Position.X + 4)
+                            // if the projectile x and y postions intersect with the enemy x and y postions then remove both from the list
+                            {
+                                Enemies[j].Hit(Projectiles[i]);
+                                Projectiles.RemoveAt(i);
+                                break;
+                            }
+                        }
                     }
-                    Console.SetCursorPosition(enemies[i].Position.X, enemies[i].Position.Y + j);
-                    Console.Write(currentEnemySprite[j]);
+                }
+                else if (Projectiles[i].TravelDirection == Direction.Down)
+                {
+                    if (Projectiles[i].Position.Y < PlayerShip.Position.Y &&
+                        Projectiles[i].Position.X >= PlayerShip.Position.X &&
+                        Projectiles[i].Position.X <= PlayerShip.Position.X + 2)
+                    {
+                        PlayerShip.Hit(Projectiles[i]);
+                    }
                 }
 
             }
-            Console.ResetColor();
+            return;
         }
-        public static void DrawPlayer(Player myPlayer)
-        {
-            for (int i = 0; i < _pepperSprite.Count; i++)
-            {
-                switch (i)
-                {
-                    default:
-                        Console.ForegroundColor = _pepperColors[0];
-                        break;
-                    case 2:
-                        Console.ForegroundColor = _pepperColors[1];
-                        break;
-                }
-                Console.SetCursorPosition(myPlayer.Position.X, myPlayer.Position.Y + i);
-                Console.Write(_pepperSprite[i]);
-            }
-            Console.ResetColor();
-        }
-        public static void DrawProjectiles(List<Projectile> projectiles)
-        {
-            List<string> currentMissileSprite = new List<string> { };
-            List<ConsoleColor> currentMissileColors = new List<ConsoleColor> { };
-            if (projectiles == null) return;
-            for (int i = 0; i < projectiles.Count; i++) 
-            {
-                Console.SetCursorPosition(projectiles[i].Position.X, projectiles[i].Position.Y);
-                switch (projectiles[i])
-                {
-                    case Bullet:
-                        Console.Write("|");
-                        break;
-                    case Laser:
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.Write("¦");
-                        Console.ResetColor();
-                        break;
-                    case Missile:
-                        switch (projectiles[i].TravelDirection)
-                        {
-                            case Direction.Up:
-                                currentMissileColors = _missileColorsUp;
-                                currentMissileSprite = _missileSpriteUp;
-                                break;
-                            case Direction.Down:
-                                currentMissileColors = _missileColorsDown;
-                                currentMissileSprite = _missileSpriteDown;
-                                break;
-                        }
-                        for (int j = 0; j < currentMissileSprite.Count; j++)
-                        {
-                            Console.SetCursorPosition(projectiles[i].Position.X, projectiles[i].Position.Y + j);
-                            Console.ForegroundColor = currentMissileColors[j];
-                            Console.Write(currentMissileSprite[j]);
-                            Console.ResetColor();
 
-                        }
-                        break;
+        /// <summary>
+        /// Method responsible for checking projectile positions and comparing them with the gameboard limits, removing projectiles if they reached a certain boundery
+        /// </summary>
+        public void CheckProjectileBounderies()
+        {
+            for (int i = 0; i < Projectiles.Count; i++)
+            {
+                if (Projectiles[i].Position.Y + Projectiles[i].Velocity.Y > GameSettings.GAMEBOARD_Y_LIMIT && Projectiles[i].TravelDirection == Direction.Down)
+                {
+                    Projectiles.RemoveAt(i);
+                }
+                else if (Projectiles[i].Position.Y - Projectiles[i].Velocity.Y <= 0 && Projectiles[i].TravelDirection == Direction.Up)
+                {
+                    Projectiles.RemoveAt(i);
                 }
             }
         }
-        
-        public static void Explode(SmartEntity entityToExplode) 
+
+        /// <summary>
+        /// Method responsible for making sure the player is within gameboard bounderies
+        /// </summary>
+        /// <param name="direction">The direction the player is trying to move in</param>
+        /// <returns>Returns true player has reached a limit, otherwise false</returns>
+        public bool CheckPlayerBounderies(Direction direction)
         {
+            if (direction == Direction.Left && !(PlayerShip.Position.X > GameSettings.GAMEBOARD_X_START))
+            {
+                return true;
+            }
+            else if (direction == Direction.Right && !(PlayerShip.Position.X < GameSettings.GAMEBOARD_X_LIMIT))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        ///  Method responsible translating key inputs into player actions.
+        /// </summary>
+        public void PlayerControls()
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                Direction direction = Direction.None;
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.A:
+                        direction = Direction.Left;
+                        break;
+                    case ConsoleKey.D:
+                        direction = Direction.Right;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        direction = Direction.Left;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        direction = Direction.Right;
+                        break;
+                    case ConsoleKey.Spacebar:
+                        Projectiles.Add(PlayerShip.Shoot());
+                        break;
+                }
+                if (!CheckPlayerBounderies(direction))
+                {
+                    PlayerShip.Move(direction);
+                }
+            }
         }
     }
 }
