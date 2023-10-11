0d0774e441cdc1ace4695fa8239700575820bf0e
ethanschafstall <ethans9999@gmail.com>
2023-10-11 17:07:55 +0200

    Added Player Class
    Added a player class for the person playing the game.

---
+++ OO/Spicy_Invaders1/ConsoleApp1/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spicy_Invaders
+{
+    public class Player
+    {
+        public int Score { get; set; }
+        public string Alias { get;}
+
+        public Player(string alias) 
+        {
+            this.Alias = alias;
+        }
+    }
+}
