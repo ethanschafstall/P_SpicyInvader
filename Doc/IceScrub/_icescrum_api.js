1a1a56886aee8134ef4764f8474fcf0e3e135c6b
ethanschafstall <ethans9999@gmail.com>
2023-10-11 11:12:26 +0200

    Add files via upload

---
+++ Doc/IceScrub/_icescrum_api.js
@@ -0,0 +1,12 @@
+// A token is required to authenticate your requests
+// Get it from IceScrum > MyAccount > Account > API tokens
+var iceScrumToken = '693d543668e246849b7e10da9f61e02d' 
+
+// You can use IceScrub utils with the following list of projects
+// Get their IDs from each project > Settings > Your Project > Key
+projectIds = [ 
+    'SPICY12'
+]
+
+// Select the backend to use
+const API_base_url = "https://etml.icescrum.com/ws/project/";
