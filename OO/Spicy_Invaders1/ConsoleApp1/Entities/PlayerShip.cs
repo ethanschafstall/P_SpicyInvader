1b1708a1976d979a3b5763222f97ee2ca02d9a3a
ethanschafstall <ethans9999@gmail.com>
2023-10-11 17:10:29 +0200

    Renamed Player to PlayerShip
    Renamed player class to playership, as it's not the player but rather the ship they are controling

---
+++ OO/Spicy_Invaders1/ConsoleApp1/Entities/PlayerShip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spicy_Invaders
+{
+    public class PlayerShip : SmartEntity
+    {
+        public PlayerShip() 
+        {
+        }
+        public PlayerShip(int x, int y) 
+        {
+            Position = new Vector(x, y);
+            Velocity = new Vector(1, 1);
+            weaponType = WeaponType.Gun;
+            FaceDirection = Direction.Up;
+        }
+        public override void Hit(Projectile projectile) 
+        { 
+            IsAlive = false;
+        }
+    }
+}
