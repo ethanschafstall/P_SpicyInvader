3ecc60891890aa72c85b34f2a1492b1f949efce9
ethanschafstall <ethans9999@gmail.com>
2023-11-01 16:06:26 +0100

    Language interface and classes
    Added Language interface and 3 classes using said interface  for geolocal.

---
+++ OO/Spicy_Invaders1/ConsoleApp1/Language/ILanguage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spicy_Invaders
+{
+    public interface ILanguage
+
+    {
+        public List<string> Logo();
+        public List<string> MainMenuText(int optionNumber);
+
+        public List<string> OpitionsMenuText(int optionNumber);
+
+        public List<string> ChooseLanguageMenuText(int optionNumber);
+
+        public List<string> GameplayText();
+
+        public List<string> GameplayMenuText(int optionNumber);
+
+        public List<string> WeaponMenuText(int optionNumber);
+
+
+    }
+}
